ad75534f11b0ddd33975da831930a5c180bd5688
Ruslan <ruslan.abbasov@urfu.me>
2023-04-23 14:50:31 +0500

    doing req

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.http import HttpResponse
+import re
+
+
 # Create your views here.
 
 
@@ -8,10 +11,15 @@ def index(request):
     host = request.META["HTTP_HOST"]  # получаем адрес сервера
     user_agent = request.META["HTTP_USER_AGENT"]  # получаем данные бразера
     path = request.path  # получаем запрошенный путь
-    headers = request.headers
+    header = request.headers
+
+    regex = re.compile('^HTTP_')
+    dict((regex.sub('', header), value) for (header, value)
+         in request.META.items() if header.startswith('HTTP_'))
+
     return HttpResponse(f"""
         <p>Host: {host}</p>
         <p>Path: {path}</p>
         <p>User-agent: {user_agent}</p>
-        <p>Headers: {headers}</p>
+        <p>Headers: {regex}</p>
     """)
